c999913b0d49919613bb1508f4046392b9a93920
Hesam Mousavi <hesammoousavi@gmail.com>
2025-05-07 12:06:00 +0330

    new update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mr success App
-        </p>
+        <p>Pachim App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
